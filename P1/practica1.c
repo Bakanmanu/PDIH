a0022e294c2d9a07532a3d56eb85f1c93e357539
Amador Carmona Méndez <amadorcarmonamendez@MacBook-Pro-de-Amador.local>
2023-03-21 02:29:19 +0100

    p1- falta docu

--- P1/practica1.c
+++ P1/practica1.c
@@ -14,7 +14,9 @@
     • clrscr(): borra toda la pantalla
     • cputchar(): escribe un carácter en pantalla con el color indicado actualmente
     • getche(): obtiene un carácter de teclado y lo muestra en pantalla
-
+ Funciones Extra:
+    • recuadro(): dibuja un recuadro en la pantalla en modo texto. Recibirá como parámetros las coordenadas superior izquierda e inferior derecha del recuadro, el color de primer plano y el color de fondo.
+ 
 ***************************************************************************/
 
 
@@ -22,82 +24,37 @@
 
 #include <stdio.h>
 #include <dos.h>
+#include <string.h>
 #define BYTE unsigned char
-
 
 BYTE MODOTEXTO = 3;
 BYTE MODOGRAFICO = 4;
+
+int colortexto=7;
+int colorback=0;
+
+
 // hace una pausa
-void pausa(){
+void pausa(void){
     union REGS inregs, outregs;
+
     inregs.h.ah = 0x00;
     int86(0x16, &inregs, &outregs);
 }
-void gotoxy(int x,int y){
-    union REGS inregs, outregs;
-    inregs.h.ah=0x02;
-    inregs.h.bh=0x00;
-    inregs.h.dh=x; // Coordenada x
-    inregs.h.dl=y; // Coordenada y
-    int86(0x10, &inregs, &outregs);
-}
-void setvideomode(int x){
-    union REGS inregs, outregs;
-    inregs.h.ah=0x00;
-    inregs.h.al=x;
-    int86(0x10, &inregs, &outregs);
-}
-int getvideomode(){
-    union REGS inregs, outregs;
-    unsigned char modo;
-    inregs.h.ah = 1;
-    int86(0x10, &inregs, &outregs);
-    modo=outregs.h.al;
-    return modo;
-}
-void textcolor(int color){
-    union REGS regs;
-    regs.h.ah = 0x09;   // subcódigo para cambiar el color de texto
-    regs.h.al = color;  // color deseado
-    regs.h.bh = 0;      // página de pantalla (0 para la página actual)
-    int86(0x10, &regs, &regs);  // llamada a la interrupción de DOS
-}
-void textbackground(int color){
-    union REGS regs;
-    regs.h.ah = 0x00;   // subcódigo para cambiar el color de fondo
-    regs.h.al = color;  // color deseado
-    regs.h.bh = 0;      // página de pantalla (0 para la página actual)
-    int86(0x10, &regs, &regs);  // llamada a la interrupción de DOS
-}
-void clrscr(){
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-}
 
-// establece el modo de vídeo: 3-texto, 4-gráfico
-void modovideo(BYTE modo){
-    union REGS inregs, outregs;
-    inregs.h.al = modo;
-    inregs.h.ah = 0x00;
-    int86(0x10, &inregs, &outregs);
-}
-// pone un pixel en la coordenada X,Y de color C
-void pixel(int x, int y, BYTE C){
-    union REGS inregs, outregs;
-    inregs.x.cx = x;
-    inregs.x.dx = y;
-    inregs.h.al = C;
-    inregs.h.ah = 0x0C;
-    int86(0x10, &inregs, &outregs);
-}
-int getche(){
+
+int mi_getchar(void){
     union REGS inregs, outregs;
     int caracter;
+
     inregs.h.ah = 1;
     int86(0x21, &inregs, &outregs);
+
     caracter = outregs.h.al;
     return caracter;
 }
-void cputchar(char c){
+
+void mi_putchar(char c){
     union REGS inregs, outregs;
 
     inregs.h.ah = 2;
@@ -105,8 +62,117 @@ void cputchar(char c){
     int86(0x21, &inregs, &outregs);
 }
 
+/**
+ gotoxy(): coloca el cursor en una posición determinada
+ **/
+void gotoxy(int x,int y){
+    union REGS inregs, outregs;
+
+    inregs.h.ah=0x02;
+    inregs.h.bh=0x00;
+    inregs.h.dh=x; // Coordenada x
+    inregs.h.dl=y; // Coordenada y
+    int86(0x10, &inregs, &outregs);
+}
+/**
+ setvideomode(): fija el modo de video deseado
+ **/
+void setvideomode(int x){
+    union REGS inregs, outregs;
+
+    inregs.h.al = x;
+    inregs.h.ah = 0x00;
+    int86(0x10, &inregs, &outregs);
+}
+/**
+ getvideomode(): obtiene el modo de video actual
+ **/
+void getvideomode(void){
+    union REGS inregs, outregs;
+    unsigned char modo;
+
+    inregs.h.ah = 0xF;
+
+    int86(0x10, &inregs, &outregs);
+
+    modo=outregs.h.al;
+
+    printf("%i",modo);
+}
+/**
+ textcolor(): modifica el color de primer plano con que se mostrarán los caracteres
+ **/
+void textcolor(int color){
+    colortexto=color;
+    // union REGS inregs, outregs;
+
+    // inregs.h.ah=0x09;
+    // inregs.h.al=97;     // 'a'
+    // inregs.h.bl=color;
+    // inregs.h.bh=0x00;
+    // inregs.x.cx=10;     // numero de repeticiones;
+
+    // int86(0x10, &inregs, &outregs);
+}
+/**
+ textbackground(): modifica el color de fondo con que se mostrarán los caracteres
+ **/
+void textbackground(int color){
+    colorback=color;
+}
+/**
+ clrscr(): borra toda la pantalla
+ **/
+void clrscr(void){
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+}
+
+
+/**
+ pixel()
+ **/
+void pixel(int x, int y, BYTE C){
+    union REGS inregs, outregs;
+
+    inregs.x.cx = x;
+    inregs.x.dx = y;
+    inregs.h.al = C;
+    inregs.h.ah = 0x0C;
+    int86(0x10, &inregs, &outregs);
+}
+/**
+ getche(): obtiene un carácter de teclado y lo muestra en pantalla
+ **/
+void getche(void){
+    union REGS inregs, outregs;
+    int caracter;
+
+    caracter = mi_getchar();
+    printf("\nHas pulsado: ");
+    mi_putchar((char) caracter);
+}
+/**
+ cputchar(): escribe un carácter en pantalla con el color indicado actualmente
+ **/
+void cputchar(int c){
+    union REGS inregs, outregs;
+
+    inregs.h.ah = 0x09;
+    inregs.h.al = c;
+    // inregs.h.dl = c;
+    inregs.h.bl = colorback << 4 | colortexto;
+    inregs.h.bh = 0x00;
+    inregs.x.cx = 1;
+    int86(0x10, &inregs, &outregs);
+    // return;
+}
+/**
+ setcursortype(): fijar el aspecto del cursor, debe admitir tres valores: INVISIBLE, NORMAL y GRUESO.
+ **/
+
 void setcursortype(int tipo_cursor){
     union REGS inregs, outregs;
+
     inregs.h.ah = 0x01;
     switch(tipo_cursor){
     case 0: //invisible
@@ -122,41 +188,114 @@ void setcursortype(int tipo_cursor){
         inregs.h.cl = 010;
         break;
     case 3: //modo gráfico
-        modovideo(MODOGRAFICO); //gráfico
+        setvideomode(MODOGRAFICO); //gráfico
         pixel(10,40,0);
         pixel(10,50,1);
         pixel(15,60,2);
         pixel(20,70,3);
         pausa();
-        modovideo(MODOTEXTO); //texto
+        setvideomode(MODOTEXTO); //texto
         break;
     }
     int86(0x10, &inregs, &outregs);
     
 }
+/**
+ mi_printf(): saca por pantalla utilizando cputchar() una cadena de caracteres
+ **/
+int i=0;
+void mi_printf(const char *str){
+    printf("\n");
 
+    for(i; i < strlen(str); i++){
+        cputchar((int) str[i]);
+        printf("%c", str[i]);
+    }
+    i=0;
+}
 
+int tmp;
+/**
+ recuadro(): dibuja un recuadro en la pantalla en modo texto. Recibirá como parámetros las coordenadas superior izquierda e inferior derecha del recuadro, el color de primer plano y el color de fondo.
+ **/
+int j;
+int color;
+void recuadro(int x1,int y1, int x2, int y2,int cp, int cf){
+    color=cf << 4 | cp;
+    setvideomode(4);
+    i=x1;
+    j=y1;
+    for(i; i<=x2;i++){
+        for(j;j<=y2;j++){
+            
+            pixel(i,j,cp);
+        }
+        j=y1;
+        //printf("\n");
+        
+    }
+    
+}
+/**
+ main(): funcion principal deonde probamos todas las funciones
+ **/
 int main(){
-    clrscr();
-    textcolor(0x01);   // cambiar el color de texto a verde
-    printf("Hola mundo0000000000000!\n");
-    int tmp;
-    printf("\nPulsa una tecla... ");
-    tmp = getchar();
-    printf("\nHas pulsado: ");
-    putchar( (char)tmp );
-    printf("\nCursor invisible: ");
+    
+    mi_printf("Pulsa una tecla: ");
+    getche();
+    mi_printf("Cursor invisible: ");
     setcursortype(0);
-    //pausa();
-    printf("\nCursor grueso: ");
+    pausa();
+    
+    mi_printf("Cursor grueso: ");
     setcursortype(2);
-    //pausa();
-    printf("\nCursor normal: ");
+    pausa();
+    
+    mi_printf("Cursor normal: ");
     setcursortype(1);
-    //pausa();
+    pausa();
+    
+    mi_printf("Probando gotoxy: ");
+    gotoxy(20,22);
+    pausa();
+    
+    mi_printf("Probando set videomode a modo grafico: ");
+    setvideomode(4);
+    pixel(10,40,0);
+    pixel(10,50,1);
+    pixel(15,60,2);
+    pixel(20,70,3);
+    pausa();
+    setvideomode(3);
+    pausa();
+    
+    mi_printf("Probando set videomode a modo texto: ");
+    
+    mi_printf("Probando set getvideomode: ");
+    getvideomode();
+    mi_printf("Leyenda: ");
+    mi_printf("Si es menor que 3 o igual a 7 = texto"); 
+    mi_printf("En el resto de casos: = Grafico ");
+    
+    textcolor(1);
+    mi_printf("Color de texto cambiado");
+    pausa();
+    
+    textbackground(11);
+    mi_printf("Color de fondo cambiado");
+    pausa();
     
     clrscr();
-    textbackground(0x0B);   // cambiar el color de fondo a cyan
-    printf("Holaaaaaaa!\n");
+    mi_printf("pantalla limpiada");
+    
+    pausa();
+    mi_printf("imprimiendo rectangulo que empieza en el pixel (10,10) y termina en (50,60)");
+    recuadro(30,20,50,60,1,3);
+    
+
+    pausa();
+    setvideomode(3);
+    
     return 0;
 }
+
